3b243ebd774ec5f5ee3238267975b92965ef91bc
Hugo Saporetti Junior <yorevs@gmail.com>
2018-10-02 16:17:48 -0300

    ++Version check message

--- bashrc.sh
+++ bashrc.sh
@@ -12,6 +12,8 @@
 if [ -f "$HOME_SETUP/VERSION" ]
 then
     export DOTFILES_VERSION=$(cat "$HOME_SETUP/VERSION")
+    v=$(curl -s -m 2 https://raw.githubusercontent.com/yorevs/homesetup/master/VERSION)
+    test "$DOTFILES_VERSION" != "$v" && echo "${RED}You have a different version of HomeSetup (Repo: $v , Yours: $DOTFILES_VERSION). Pull the repository to get updated!${NC}"
 fi
 
 if [ -f /etc/bashrc ]
