17129d2e51265211f89b65cd6ada9c7e8fd95457
Hugo Saporetti Junior <yorevs@gmail.com>
2018-10-25 15:46:05 -0300

    App. dotfiles.sh - part 1

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -16,7 +16,11 @@ PROC_NAME=$(basename "$0")
 
 # Help message to be displayed by the script.
 USAGE="
-Usage: $PROC_NAME [optionals] <mandatories>
+Usage: $PROC_NAME <command> [<args>]
+
+    Commands:
+        FB | firebase   : Execute Firebase tasks; type: '$PROC_NAME help FB' for details.
+        H  | help       : Provides a help about the command.
 "
 
 # Import pre-defined .bash_colors
@@ -27,14 +31,14 @@ test -f ~/.bash_colors && source ~/.bash_colors
 # @param $1 [Req] : The exit return code.
 # @param $2 [Opt] : The exit message to be displayed.
 quit() {
-    
+
     test "$1" != '0' -a "$1" != '1' && printf "%s" "${RED}"
     test -n "$2" -a "$2" != "" && printf "%s\n" "${2}"
     test "$1" != '0' -a "$1" != '1' && printf "%s" "${NC}"
 
     # Unset all declared functions
-    unset -f quit usage version 
-    
+    unset -f quit usage version exec_command cmd_help cmd_firebase
+
     exit "$1"
 }
 
@@ -52,21 +56,106 @@ version() {
 test "$1" = '-h' -o "$1" = '--help' && usage
 test "$1" = '-v' -o "$1" = '--version' && version
 
-# Loop through the command line options.
-# Short opts: -<C>, Long opts: --<Word>
-while test -n "$1"
-do
-    case "$1" in
-        -s | --stuff)
-            shift
-            # Do stuff
+# Execute a dotfiles command.
+exec_command() {
+
+    shopt -s nocasematch
+    case "${COMMAND}" in
+        # Do stuff related to firebase
+        cmd_firebase)
+            cmd_firebase "$@"
         ;;
-        
+        cmd_help)
+            cmd_help "$@"
+        ;;
         *)
-            quit 1 "Invalid option: \"$1\""
+            quit 1 "Invalid command \"${COMMAND}\" !"
         ;;
     esac
-    shift
-done
+    shopt -u nocasematch
+    quit 0
+}
 
-usage
+# Provides a help about the command.
+cmd_help() {
+    shopt -s nocasematch
+    case "$1" in
+        # Do stuff related to firebase
+        FB | firebase)
+            echo "Execute firebase synchronization tasks."
+            echo "Usage: $PROC_NAME firebase <task> [args]"
+            echo ""
+            echo "  Tasks:"
+            echo "        setup               : Setup your Firebase account to use with your HomeSetup installation."
+            echo "       upload <db_alias>    : Upload your custom .dotfiles to your Firebase 'Realtime Database'."
+            echo "     download <db_alias>    : Download your custom .dotfiles from your Firebase 'Realtime Database'."
+            echo "        merge <db_alias>    : Merge your custom .dotfiles with the ones at your Firebase 'Realtime Database'."
+        ;;
+        H | help)
+            echo "Provides a help to the given command."
+            echo "Usage: $PROC_NAME help <command>"
+        ;;
+        *)
+            quit 1 "Command \"$1\" does not exist!"
+        ;;
+    esac
+    shopt -u nocasematch
+}
+
+# Execute a Firebase command
+cmd_firebase() {
+
+    task="$1"
+    shift
+    args=( "$@" )
+
+    echo "T: $task"
+    echo "A: ${args[*]}"
+
+    test -z "${args[*]}" -a "$task" != 'setup' && quit 2 "Invalid number of arguments for task: \"$task\" !"
+
+    shopt -s nocasematch
+    case "$task" in
+        setup)
+            echo "TODO Setup"
+        ;;
+        upload)
+            echo "TODO Upload"
+        ;;
+        download)
+            echo "TODO Download"
+        ;;
+        merge)
+            echo "TODO Merge"
+        ;;
+        *)
+            quit 2 "Invalid firebase task: \"$task\" !"
+        ;;
+    esac
+    shopt -u nocasematch
+}
+
+test -z "$1" && usage
+
+shopt -s nocasematch
+# Loop through the command line options.
+case "$1" in
+    # Do stuff related to firebase
+    FB | firebase)
+        COMMAND="cmd_firebase"
+        shift
+    ;;
+    # Help about the command
+    H | help)
+        COMMAND="cmd_help"
+        shift
+    ;;
+    *)
+        quit 1 "Invalid option: \"$1\""
+    ;;
+esac
+shopt -u nocasematch
+
+exec_command "$@"
+
+quit 0
--- bin/fetch.sh
+++ bin/fetch.sh
@@ -35,14 +35,14 @@ test -f ~/.bash_colors && source ~/.bash_colors
 # @param $1 [Req] : The exit return code.
 # @param $2 [Opt] : The exit message to be displayed.
 quit() {
-    
+
     test "$1" != '0' -a "$1" != '1' && printf "%s" "${RED}"
     test -n "$2" -a "$2" != "" && printf "%s\n" "${2}"
     test "$1" != '0' -a "$1" != '1' && printf "%s" "${NC}"
 
     # Unset all declared functions
     unset -f quit usage version format_json do_fetch
-    
+
     exit "$1"
 }
 
@@ -63,40 +63,38 @@ test -z "$1" && usage
 
 shopt -s nocasematch
 case "$1" in
-    'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE')
-        METHOD="$(echo "$1" | tr '[:lower:]' '[:upper:]')"
-        shift
+'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE')
+    METHOD="$(echo "$1" | tr '[:lower:]' '[:upper:]')"
+    shift
     ;;
-    *)
-        quit 2 "Method $1 is not not valid!"
+*)
+    quit 2 "Method $1 is not not valid!"
     ;;
 esac
 shopt -u nocasematch
 
 # Loop through the command line options.
-# Short opts: -<C>, Long opts: --<Word>
-while test -n "$1"
-do
+while test -n "$1"; do
     case "$1" in
-        --headers)
-            shift
-            IFS=','
-            arr=( "$1" )
-            for h in ${arr[*]}; do
-                HEADERS="$HEADERS -H $h"
-            done
-            IFS=' '
+    --headers)
+        shift
+        IFS=','
+        arr=("$1")
+        for h in ${arr[*]}; do
+            HEADERS="$HEADERS -H $h"
+        done
+        IFS=' '
         ;;
-        --body)
-            shift
-            BODY="$1"
+    --body)
+        shift
+        BODY="$1"
         ;;
-        --format)
-            FORMAT=1
+    --format)
+        FORMAT=1
         ;;
-        *)
-            URL="$*"
-            break
+    *)
+        URL="$*"
+        break
         ;;
     esac
     shift
@@ -106,13 +104,13 @@ test -z "$URL" && quit 2 "No URL was defined!"
 
 if [ "GET" = "${METHOD}" ] || [ "DELETE" = "${METHOD}" ]; then
     test -n "${BODY}" && quit 2 "${METHOD} does not accept any body"
-elif [ "PUT" = "${METHOD}" ] || [ "POST" = "${METHOD}" ]|| [ "PATCH" = "${METHOD}" ]; then
+elif [ "PUT" = "${METHOD}" ] || [ "POST" = "${METHOD}" ] || [ "PATCH" = "${METHOD}" ]; then
     test -z "${BODY}" && quit 2 "${METHOD} requires a body"
 fi
 
 # Format or not the output
 format_json() {
-    
+
     # Piped input
     read -r response
     test -n "${FORMAT}" && echo "$response" | json_pp -f json -t json -json_opt pretty indent escape_slash
@@ -121,11 +119,11 @@ format_json() {
 
 # Do the request
 do_fetch() {
-    
-    test -z "$HEADERS" -a -z "${BODY}" && curl -X "${METHOD}" "${URL}"  2>/dev/null | format_json
-    test -z "$HEADERS" -a -n "${BODY}" && curl -X "${METHOD}" -d "${BODY}" "${URL}"  2>/dev/null | format_json
-    test -n "$HEADERS" -a -n "${BODY}" && curl -X "${METHOD}" -d "${BODY}" "${URL}"  2>/dev/null | format_json
-    test -n "$HEADERS" -a -z "${BODY}" && curl -X "${METHOD}" "${URL}"  2>/dev/null | format_json
+
+    test -z "$HEADERS" -a -z "${BODY}" && curl -X "${METHOD}" "${URL}" 2>/dev/null | format_json
+    test -z "$HEADERS" -a -n "${BODY}" && curl -X "${METHOD}" -d "${BODY}" "${URL}" 2>/dev/null | format_json
+    test -n "$HEADERS" -a -n "${BODY}" && curl -X "${METHOD}" -d "${BODY}" "${URL}" 2>/dev/null | format_json
+    test -n "$HEADERS" -a -z "${BODY}" && curl -X "${METHOD}" "${URL}" 2>/dev/null | format_json
 }
 
 echo "Fetching (${METHOD}) $URL ..."
