e01af26dc483bf9f434881088a5ceb016424b2f7
Hugo Saporetti Junior <yorevs@gmail.com>
2018-10-25 13:14:15 -0300

    Adjustments

--- bin/toolcheck.sh
+++ bin/toolcheck.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1117
 
 #  Script: toolcheck.sh
 # Purpose: Check if the a tool is installed on the system.
@@ -17,16 +18,23 @@ USAGE="
 Usage: $PROC_NAME <tool_name> [exit_on_error 'true/[false]']
 "
 
+# Import pre-defined .bash_colors
+# shellcheck disable=SC1090
+test -f ~/.bash_colors && source ~/.bash_colors
+
 # Purpose: Quit the program and exhibits an exit message if specified.
 # @param $1 [Req] : The exit return code.
 # @param $2 [Opt] : The exit message to be displayed.
 quit() {
     
-    if test -n "$2" -o "$2" != ""; then
-        echo -e "$2"
-    fi
+    test "$1" != '0' -a "$1" != '1' && printf "%s" "${RED}"
+    test -n "$2" -a "$2" != "" && printf "%s\n" "${2}"
+    test "$1" != '0' -a "$1" != '1' && printf "%s" "${NC}"
 
-    exit $1
+    # Unset all declared functions
+    unset -f quit usage version 
+    
+    exit "$1"
 }
 
 # Usage message.
@@ -34,11 +42,13 @@ usage() {
     quit 1 "$USAGE"
 }
 
+# Version message.
+version() {
+    quit 1 "$VERSION"
+}
+
 # Check if the user passed the help parameters.
 test "$1" = '-h' -o "$1" = '--help' -o -z "$1" -o "$1" = "" && usage
-
-# Import bash colors
-test -f ~/.bash_colors && source ~/.bash_colors
 
 TOOL_NAME="$1"
 
@@ -51,18 +61,18 @@ fi
 PAD=$(printf '%0.1s' "."{1..60})
 PAD_LEN=20
 
-printf "${ORANGE}($(uname -s))${NC} "
-printf "Checking: ${YELLOW}${TOOL_NAME}${NC} "
+printf '%s\n' "${ORANGE}($(uname -s))${NC} "
+printf '%s\n' "Checking: ${YELLOW}${TOOL_NAME}${NC} "
 printf '%*.*s' 0 $((PAD_LEN - ${#1})) "$PAD"
 
-CHECK=$(command -v ${TOOL_NAME})
+CHECK=$(command -v "${TOOL_NAME}")
 
 if test -n "${CHECK}" ; then
-    printf "${GREEN}INSTALLED${NC} at ${CHECK}\n"
+    printf '%s\n' "${GREEN}INSTALLED${NC} at ${CHECK}\n"
 else
-    printf "${RED}NOT INSTALLED${NC}\n"
+    printf '%s\n' "${RED}NOT INSTALLED${NC}\n"
     if test "$EXIT_ON_FAIL" = "true"; then
-        printf "${RED}### Error: Unable to continue without the required tool: \"${TOOL_NAME}\"${NC}\n" >&2
+        printf '%s\n' "${RED}### Error: Unable to continue without the required tool: \"${TOOL_NAME}\"${NC}\n" >&2
         quit 2
     else
         quit 1
