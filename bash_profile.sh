20ea0014485fbf5675b2f493fb47c17f8e1c9cf7
Hugo Saporetti Junior <yorevs@gmail.com>
2018-12-30 00:08:57 -0200

    Adding completion-ignore-case to .inputrc

--- bash_profile.sh
+++ bash_profile.sh
@@ -7,21 +7,29 @@
 #  Author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior
 #  Mailto: yorevs@hotmail.com
 #    Site: https://github.com/yorevs/homesetup
-# !NOTICE: Do not change this file. To customize your aliases edit the file ~/.profile
+# !NOTICE: Do not change this file. To customize your aliases edit the file `~/.profile`
 
 # inspiRED by: https://github.com/mathiasbynens/dotfiles
 
 export HOME=${HOME:-~/}
 export USER=${USER:-$(whoami)}
 
-# Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob;
+# If set, bash matches filenames in a case-insensitive fashion when performing pathname expansion.
+shopt -u nocaseglob
 
-# Use extended globbing
+# If set, the extended pattern matching features described above under Pathname Expansion are enabled.
 shopt -s extglob
 
-# Autocorrect typos in path names when using `cd`
-shopt -s cdspell;
+# If set, minor errors in the spelling of a directory component in a cd command will be corrected.
+shopt -s cdspell
+
+# If set, bash matches patterns in a case-insensitive fashion when  performing  matching while executing case or [[ conditional commands.
+shopt -u nocasematch
+
+# This turns off the case-sensitive completion.
+[ -f ~/.inputrc ] || echo "set completion-ignore-case On" > ~/.inputrc
+[ "Darwin" = "$(uname -s)" ] && sed -i '' -E "s#(^set completion-ignore-case .*)*#set completion-ignore-case On#g" ~/.inputrc
+[ "Linux" = "$(uname -s)" ] && sed -i'' -r "s#(^set completion-ignore-case .*)*#set completion-ignore-case On#g" ~/.inputrc
 
 # Load the shell dotfiles, and then:
 #   source -> ~/.path can be used to extend `$PATH`.
@@ -31,25 +39,27 @@ shopt -s cdspell;
 #   source -> ~/.colors can be used to extend/override .bash_colors
 #   source -> ~/.functions can be used to extend/override .bash_functions
 
+# Install and load all dotfiles. Custom dotfiles comes last, so defaults can be overriden.
 for file in ~/.{bash_env,bash_colors,bash_aliases,bash_prompt,bash_functions,env,aliases,profile,colors,functions}; do
     [ -r "$file" ] && [ -f "$file" ] && source "$file";
 done;
-unset file;
+
+unset file
 
 # Enable tab completion for `git` by marking it as an alias for `git`
 if [ -n "$(command -v git)" ]; then
     if type _git &> /dev/null && [ -f ~/bin/git-completion.sh ]; then
-        complete -o default -o nospace -F _git g;
+        complete -o default -o nospace -F _git g
     fi;
 fi;
 
-# Add `~/bin` to the `$PATH`
-export PATH="$PATH:$HOME/bin";
+# Add `~/bin` to the system `$PATH`
+export PATH="$PATH:$HOME/bin"
 
-# Add custom paths to $PATH
+# Add custom paths to the system `$PATH`
 if [ -f ~/.path ]; then
     export PATH="$(grep . ~/.path | tr '\n' ':'):$PATH"
 fi
 
-# Remove all $PATH duplicates
+# Remove all `$PATH` duplicates
 export PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: '!arr[$0]++')
