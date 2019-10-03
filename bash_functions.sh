045d6f2389401b670fd41bd196cd368668e0e3d8
Hugo Saporetti Junior <yorevs@gmail.com>
2019-10-03 13:32:22 -0300

    Improved tl patterns

--- bash_functions.sh
+++ bash_functions.sh
@@ -525,17 +525,21 @@ function __hhs_tc() {
 # @param $1 [Req] : The log file name.
 function __hhs_tailor() {
 
-    [ -f "$HHS_DIR/.tailor" ] || printf '%s\n' "
-THREAD_NAME_RE='\[(.*main.*|Thread-[0-9]+)\]'
+    echo -e "
+THREAD_NAME_RE=\"\[ ?(.*main.*|Thread-[0-9]*) ?\] \"
 THREAD_NAME_STYLE=\"${VIOLET}\"
-FQDN_RE='(([a-zA-Z][a-zA-Z0-9]*\.)+[a-zA-Z0-9]*)'
+FQDN_RE=\"(([a-zA-Z][a-zA-Z0-9]*\.)+[a-zA-Z0-9]*)\"
 FQDN_STYLE=\"${CYAN}\"
-LOG_LEVEL_RE='INFO|SEVERE|FATAL|DEBUG|WARN'
-LOG_LEVEL_STYLE=\"${GREEN}\"
-DATE_FMT_RE='([0-9]{2,4}\-?)+ ([0-9]{2}\:?)+\.[0-9]{3}'
+LOG_LEVEL_RE=\"INFO|DEBUG|FINE\"
+LOG_LEVEL_STYLE=\"${BLUE}\"
+LOG_LEVEL_WARN_RE=\"WARN|WARNING\"
+LOG_LEVEL_WARN_STYLE=\"${YELLOW}\"
+LOG_LEVEL_ERR_RE=\"SEVERE|FATAL|ERROR\"
+LOG_LEVEL_ERR_STYLE=\"${RED}\"
+DATE_FMT_RE=\"([0-9]{2,4}\-?)* ([0-9]{2}\:?)+\.[0-9]{3}\"
 DATE_FMT_STYLE=\"${DIM}\"
-PATH_FMT_RE='(((http|https|ftp|file)\:\/)?\/[a-zA-Z0-9]+\/?([a-zA-Z0-9]+\/?)+[^ \.]*)'
-PATH_FMT_STYLE=\"${ORANGE}\"
+URI_FMT_RE=\"(([a-zA-Z0-9+.-]+:\/|[a-zA-Z0-9+.-]+)?\/([a-zA-Z0-9.\-_/]*)(:([0-9]+))?\/?([a-zA-Z0-9.\-_/]*)?)\"
+URI_FMT_STYLE=\"${ORANGE}\"
     " > "$HHS_DIR/.tailor" && source "$HHS_DIR/.tailor"
 
     if [ -z "$1" ] || [ ! -f "$1" ]; then
@@ -546,8 +550,10 @@ PATH_FMT_STYLE=\"${ORANGE}\"
             -e "s/(${THREAD_NAME_RE})/${THREAD_NAME_STYLE}\1${NC}/" \
             -e "s/(${FQDN_RE})/${FQDN_STYLE}\1${NC}/" \
             -e "s/(${LOG_LEVEL_RE})/${LOG_LEVEL_STYLE}\1${NC}/" \
+            -e "s/(${LOG_LEVEL_WARN_RE})/${LOG_LEVEL_WARN_STYLE}\1${NC}/" \
+            -e "s/(${LOG_LEVEL_ERR_RE})/${LOG_LEVEL_ERR_STYLE}\1${NC}/" \
             -e "s/(${DATE_FMT_RE})/${DATE_FMT_STYLE}\1${NC}/" \
-            -e "s/(${PATH_FMT_RE})/${PATH_FMT_STYLE}\1${NC}/"
+            -e "s/(${URI_FMT_RE})/${URI_FMT_STYLE}\1${NC}/"
         return $?
     fi
 }
