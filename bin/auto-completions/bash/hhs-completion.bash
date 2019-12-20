d275862ce9856f5970de746ce96f1b1c37f77dbe
Hugo Saporetti Junior <yorevs@gmail.com>
2019-12-20 14:56:09 -0300

    Adjustments for auto-completions - [DEVELOP:{@hjunior}]

--- bin/auto-completions/bash/hhs-completion.bash
+++ bin/auto-completions/bash/hhs-completion.bash
@@ -2,10 +2,12 @@
 
 __hhs_complete() {
 
-  [ "${#COMP_WORDS[@]}" != "2" ] && return
+  local all suggestions=()
+
+  [ "${#COMP_WORDS[@]}" != "2" ] && return 0
   
-  local all=(${*/%\ */})
-  local suggestions=($(compgen -W "${all[*]}" -- "${COMP_WORDS[1]}"))
+  all=(${*/%\ */})
+  suggestions=($(compgen -W "${all[*]}" -- "${COMP_WORDS[1]}"))
 
   if [ "${#all[@]}" == "1" ]; then
     local reply="${all[0]/%\ */}"
@@ -13,6 +15,8 @@ __hhs_complete() {
   else
     COMPREPLY=("${suggestions[@]}")
   fi
+
+  return 0
 }
 
 __hhs_comp-load-dir() {
@@ -20,6 +24,8 @@ __hhs_comp-load-dir() {
   local suggestions=($(grep . "$HHS_SAVED_DIRS" | awk -F'=' '{print $1}'))
 
   __hhs_complete "${suggestions[@]}"
+
+  return $?
 }
 
 __hhs_comp-aliases() {
@@ -27,6 +33,8 @@ __hhs_comp-aliases() {
   local suggestions=($(grep -v '^#' "$HOME/.aliases" | cut -d ' ' -f2- | awk -F'=' '{print $1}'))
 
   __hhs_complete "${suggestions[@]}"
+
+  return $?
 }
 
 __hhs_comp-cmd() {
@@ -34,18 +42,37 @@ __hhs_comp-cmd() {
   local suggestions=($(sed -E 's/^Command ([A-Z0-9_]*):(.*)?/\1/' "$HHS_CMD_FILE"))
 
   __hhs_complete "${suggestions[@]}"
+
+  return $?
 }
 
 __hhs_comp-godir() {
 
-  [ "${#COMP_WORDS[@]}" != "2" ] && return
+  local dir base suggestions=()
 
-  local dir="${COMP_WORDS[1]}"
-  local suggestions=($(find -L . -maxdepth 3 -type d -iname "${dir}" 2>/dev/null | sed 's/\.\///g'))
+  [ "${#COMP_WORDS[@]}" != "2" ] && return 0
+  [ -d "${COMP_WORDS[1]}" ] && return 1
+  
+  dir="$(dirname "${COMP_WORDS[1]:-.}")"
+  base="$(basename "${COMP_WORDS[1]:-*}")"
+
+  if [ -d "${dir}" ] && [ -d "${base}" ]; then
+    dir="${dir%/}/${base}"
+    base="*"
+  elif [ -d "${dir}" ] && [ ! -d "${base}" ]; then
+    base="${base}*"
+  else
+    return 1
+  fi
+
+  suggestions=($(find -L "${dir%/}" -maxdepth 3 -type d -iname "${base}" 2>/dev/null | sed -E 's#\./(.*)/*#\1/#'))
+  [ ${#suggestions[@]} -lt 2 ] && return 0
   COMPREPLY=("${suggestions[@]}")
+
+  return 0
 }
 
-complete -F __hhs_comp-load-dir load
-complete -F __hhs_comp-aliases aa
-complete -F __hhs_comp-cmd cmd
-complete -F __hhs_comp-godir go
+complete -o default -F __hhs_comp-load-dir load
+complete -o default -F __hhs_comp-aliases aa
+complete -o default -F __hhs_comp-cmd cmd
+complete -o default -F __hhs_comp-godir go
