c4934fbd19c1387d9bac1217ad4c2c4cb884de03
Hugo Saporetti Junior <yorevs@gmail.com>
2020-01-16 20:30:41 -0300

    MChoose draft - non-Functional - [DEVELOP:{@hjunior}]

---
+++ bin/hhs-functions/bash/hhs-mchoose.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#  Script: hhs-mchoose.bash
+# Created: Jan 16, 2020
+#  Author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior
+#  Mailto: yorevs@hotmail.com
+#    Site: https://github.com/yorevs/homesetup
+# License: Please refer to <http://unlicense.org/>
+# !NOTICE: Do not change this file. To customize your functions edit the file ~/.functions
+
+# @function: Select an option from a list using a navigable menu.
+# @param $1 [Req] : The response file.
+# @param $2 [Req] : The array of options.
+function __hhs_mchoose() {
+  
+  echo "TODO"
+}
