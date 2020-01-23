5544913b2f22bb0421ba54a084efd4d6e57650d2
Hugo Saporetti Junior <yorevs@gmail.com>
2020-01-23 06:42:49 -0300

    Fix the arithmetic operation invalid bug

--- bin/hhs-functions/bash/hhs-minput.bash
+++ bin/hhs-functions/bash/hhs-minput.bash
@@ -12,18 +12,21 @@
 # rl; \rm -f /tmp/out.txt; minput /tmp/out.txt "Name:input:alpha:10/30:rw:Hugo" "Password:password:any:8/30:rw:1234" "Age:input:num:1/3::41" "Role:::5:r:Admin"
 
 function __hhs_cursor_position() {
+
   exec < /dev/tty
   oldstty=$(stty -g)
   stty raw -echo min 0
-  # on my system, the following line can be replaced by the line below it
   echo -en "\033[6n" > /dev/tty
-  # tput u7 > /dev/tty    # when TERM=xterm (and relatives)
   IFS=';' read -r -d R -a pos
   stty "$oldstty"
-  [ "${1}" = "row" ] && echo "$((${pos[0]:2} - 1))"
-  [ "${1}" = "col" ] && echo "$((pos[1] - 1))"
-
-  return 0
+  if [[ ${#pos} -gt 0 ]]; then
+    vertical=${pos[0]:2}
+    horizontal=${pos[1]:-1}
+    [ "${1}" = "row" ] && [[ $vertical =~ ^[1-9]+$ ]] && echo "$((vertical - 1))" && return 0
+    [ "${1}" = "col" ] && [[ $horizontal =~ ^[1-9]+$ ]] && echo "$((horizontal - 1))"  && return 0
+  fi
+  
+  return 1
 }
 
 # @function: Select an option from a list using a navigable menu.
@@ -32,13 +35,10 @@ function __hhs_cursor_position() {
 function __hhs_minput() {
 
   echo '' > /tmp/minput.log # Reset logs
-  
+
   BLACK_BG='\033[40m'
   BLUE_BG='\033[44m'
-  SEL_COLOR='\033[1;36m'
-  UNSEL_COLOR='\033[0;97m'
-  RO_COLOR='\033[0;97m'
-  
+
   if [[ $# -eq 0 ]] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo "Usage: ${FUNCNAME[0]} <output_file> <fields...>"
     echo ''
@@ -71,9 +71,9 @@ function __hhs_minput() {
     return 1
   fi
 
-  local input_pad len re_render=1 all_fields=() outfile="$1" cur_field=() field_parts=()
-  local f_label f_mode f_type f_max_min_len f_perm f_value f_row f_col label_size=10 value_size=30
-  local minlen maxlen cur_index tab_index cur_row cur_col exit_row exit_col err_msg
+  local outfile="$1" re_render=1 label_size=10 value_size=30 all_fields=() cur_field=() field_parts=()
+  local f_label f_mode f_type f_max_min_len f_perm f_value f_row f_col
+  local len minlen maxlen cur_index tab_index cur_row cur_col exit_row exit_col err_msg
 
   if [ -d "$1" ] || [ -s "$1" ]; then
     echo -e "${RED}\"$1\" is a directory or an existing non-empty file !${NC}"
@@ -82,17 +82,15 @@ function __hhs_minput() {
 
   shift
   all_fields=(${@})
-  input_pad=$(printf '%0.1s' " "{1..100})
   len=${#all_fields[*]}
   save-cursor-pos
   disable-line-wrap
   tab_index=0
 
   while :; do
-
+    
     # Menu Renderization {
     if [ -n "$re_render" ]; then
-      echo "[DEBUG] Render started" >> /tmp/minput.log
       hide-cursor
       # Restore the cursor to the home position
       restore-cursor-pos
@@ -114,20 +112,23 @@ function __hhs_minput() {
         minlen=${f_max_min_len%/*}
         maxlen=${f_max_min_len##*/}
         f_value="${field_parts[5]}"
-        [[ $tab_index -ne $cur_index ]] || printf "${BLUE_BG}%${label_size}s: " "${f_label}"
-        [[ $tab_index -eq $cur_index ]] || printf "${BLACK_BG}%${label_size}s: " "${f_label}"
-        f_row="$(__hhs_cursor_position row)"
-        f_col="$(__hhs_cursor_position col)"
-        f_col="$((f_col + ${#f_value}))"
+        [[ $tab_index -ne $cur_index ]] && printf "${BLACK_BG}%${label_size}s: " "${f_label}"
+        if [[ $tab_index -eq $cur_index ]]; then
+          printf "${BLUE_BG}%${label_size}s: " "${f_label}"
+          f_row="$(__hhs_cursor_position row)"
+          f_col="$(__hhs_cursor_position col)"
+          f_col="$((f_col + ${#f_value}))"
+        fi
         [ "input" = "${f_mode}" ] && printf "%-${value_size}s" "${f_value}"
         [ "password" = "${f_mode}" ] && printf "%-${value_size}s" "$(sed -E 's/./\*/g' <<< "${f_value}")"
-        # printf " %*.*s " 0 $((maxlen - value_size)) "${input_pad}"
-        printf "(%${#maxlen}d/%${#maxlen}d)\n" "${#f_value}" "${maxlen}"
-        [ -n "${err_msg}" ] && echo -e "${RED}### ${err_msg}${NC}" && sleep 1
-        echo ''
-        {
-          printf "[DEBUG] IDX: %-3s \tTAB-IDX: %-3s \tL: %-20s V: \"%-20s\" \tLen: %s \tM: %8s \tT: %5s \tMin: %2s \tMax: %2s \tP: %2s \Pos: (%s,%s) \n" "${cur_index}" "${tab_index}" "${f_label}" "${f_value}" "${#f_value}" "${f_mode}" "${f_type}" "${minlen}" "${maxlen}" "${f_perm}" "${f_row}" "${f_col}"
-        } >> /tmp/minput.log
+        printf "(%${#maxlen}d/%${#maxlen}d)" "${#f_value}" "${maxlen}"
+        if [[ $tab_index -eq $cur_index ]] && [ -n "${err_msg}" ]; then
+          printf "${BLACK_BG}${RED}      <<< %s" "${err_msg}"
+          sleep 2
+          echo -en "\033[$((10 + ${#err_msg}))D"
+          unset err_msg
+        fi
+        echo -e "\033[0K\n"
         # Keep the selected field on hand
         if [[ $tab_index -eq $cur_index ]]; then
           cur_field=(${field_parts[@]})
@@ -136,7 +137,7 @@ function __hhs_minput() {
         fi
       done
       echo -e "${BLACK_BG}"
-      echo -en "${YELLOW}[Enter] Submit [↑↓] Navigate [Esc] Quit \033[0K"
+      echo -en "${YELLOW}[Enter] Submit [TAB] Next [↑↓] Navigate [Esc] Quit \033[0K"
       echo -en "${NC}"
       # Save the exit cursor position
       exit_row="$(__hhs_cursor_position row)"
@@ -144,13 +145,12 @@ function __hhs_minput() {
       re_render=
       # Position the cursor on the current tab index
       tput cup "${cur_row}" "${cur_col}"
-      show-cursor
-      echo "[DEBUG] Render finished" >> /tmp/minput.log
     fi
     IFS="$HHS_RESET_IFS"
     # } Menu Renderization
 
     # Navigation input {
+    show-cursor
     IFS= read -rsn1 KEY_PRESS
     case "$KEY_PRESS" in
       $' ' | [a-zA-Z0-9])
@@ -160,7 +160,6 @@ function __hhs_minput() {
           cur_field[5]="${cur_field[5]}${KEY_PRESS}"
           all_fields[$tab_index]="${cur_field[0]}:${cur_field[1]}:${cur_field[2]}:${cur_field[3]}:${cur_field[4]}:${cur_field[5]}"
           re_render=1
-          echo -e "[DEBUG] +CUR_FIELDS: \"${all_fields[$tab_index]}\"" >> /tmp/minput.log
         fi
         ;;
       $'\177') # Backspace
@@ -169,13 +168,14 @@ function __hhs_minput() {
           cur_field[5]="${cur_field[5]::${#cur_field[5]}-1}"
           all_fields[$tab_index]="${cur_field[0]}:${cur_field[1]}:${cur_field[2]}:${cur_field[3]}:${cur_field[4]}:${cur_field[5]}"
           re_render=1
-          echo -e "[DEBUG] -CUR_FIELDS: \"${all_fields[$tab_index]}\"" >> /tmp/minput.log
+        elif [ "r" = "${cur_field[4]}" ]; then
+          err_msg="This field is read only !"
+          re_render=1
         fi
         ;;
       $'\011') # TAB => same as Down arrow
         if [[ $((tab_index + 1)) -lt $len ]]; then
           tab_index=$((tab_index + 1)) && re_render=1
-          echo "[DEBUG] TAB pressed" >> /tmp/minput.log
         fi
         ;;
       $'\033') # Handle escape '\e[nX' codes
@@ -184,17 +184,15 @@ function __hhs_minput() {
           [A) # Cursor up
             if [[ $((tab_index - 1)) -ge 0 ]]; then
               tab_index=$((tab_index - 1)) && re_render=1
-              echo "[DEBUG] CURSOR UP" >> /tmp/minput.log
             fi
             ;;
           [B) # Cursor down
             if [[ $((tab_index + 1)) -lt $len ]]; then
               tab_index=$((tab_index + 1)) && re_render=1
-              echo "[DEBUG] CURSOR DOWN" >> /tmp/minput.log
             fi
             ;;
           *) # Escape pressed
-            if [[ ${#KEY_PRESS} -eq 1 ]]; then
+            if [[ "${#KEY_PRESS}" -eq 1 ]]; then
               echo "[WARN] ESC typed. Exit issued" >> /tmp/minput.log
               break
             fi
