df4f7c042c404deb2b69baabacf83391d593a30b
Hugo Saporetti Junior <yorevs@gmail.com>
2020-03-04 19:38:31 -0300

    Removed color-test function. Moved to the new file run-tests.bash - [DEVELOP:{@hjunior}]

--- bin/apps/bash/hhs-app/functions/built-ins.bash
+++ bin/apps/bash/hhs-app/functions/built-ins.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #  Script: built-ins.bash
-# @purpose: Contains all od the HHS-App callable functions
+# Purpose: Contains all HHS-App built-in functions
 # Created: Jan 06, 2020
 #  Author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior
 #  Mailto: yorevs@hotmail.com
@@ -44,16 +44,6 @@ function list() {
   fi
 
   quit 0 ' '
-}
-
-
-# @purpose: Execute all HomeSetup automated tests.
-function tests() {
-  command -v bats &> /dev/null || quit 1 "The tool 'bats' must be installed to run the automated tests !"
-  [[ -f "${HHS_HOME}"/tests/run-tests.bash ]] || quit 1 "Unable to find automated tests executor !"
-  "${HHS_HOME}"/tests/run-tests.bash
-
-  quit 0 ''
 }
 
 # @purpose: Search for all __hhs_functions describing it's containing file name and line number.
@@ -118,56 +108,6 @@ function host-name() {
       echo "${ORANGE}Your hostname hasn't changed !${NC}" && quit 1
     fi
   fi
-
-  quit 0
-}
-
-# @purpose: Terminal color palette test.
-function color-test() {
-
-  echo ''
-  echo "--- Home Setup color palette test"
-  echo ''
-
-  echo -en "${BLACK}  BLACK "
-  echo -en "${RED}    RED "
-  echo -en "${GREEN}  GREEN "
-  echo -en "${ORANGE} ORANGE "
-  echo -en "${BLUE}   BLUE "
-  echo -en "${PURPLE} PURPLE "
-  echo -en "${CYAN}   CYAN "
-  echo -en "${GRAY}   GRAY "
-  echo -en "${WHITE}  WHITE "
-  echo -en "${YELLOW} YELLOW "
-  echo -en "${VIOLET} VIOLET "
-  echo -e "${NC}\n"
-
-  echo "--- 16 Colors Low"
-  echo ''
-  for c in {30..37}; do
-    echo -en "\033[0;${c}mC16-${c} "
-  done
-  echo -e "${NC}\n"
-
-  echo "--- 16 Colors High"
-  echo ''
-  for c in {90..97}; do
-    echo -en "\033[0;${c}mC16-${c} "
-  done
-  echo -e "${NC}\n"
-
-  if [[ "${TERM##*-}" == "256color" ]]; then
-    echo "--- 256 Colors"
-    echo ''
-    for c in {1..256}; do
-      echo -en "\033[38;5;${c}m"
-      printf "C256-%-.3d " "${c}"
-      [[ "$(echo "$c % 12" | bc)" -eq 0 ]] && echo ''
-    done
-    echo -e "${NC}\n"
-  fi
-
-  echo ''
 
   quit 0
 }
