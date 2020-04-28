fb94d8dc16985ab7989fa1214c916f02d3ed04e9
Hugo Saporetti Junior <yorevs@gmail.com>
2020-04-28 18:24:50 -0300

    Added ssh completion - [DEVELOP:{@hjunior}]

---
+++ bin/completions/bash/ssh-completion.bash
@@ -0,0 +1,21 @@
+#  Script: ssh-completion.bash
+# Purpose: Bash completion for ssh
+# Created: Apr 28, 2020
+#  Author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior
+#  Mailto: yorevs@hotmail.com
+#    Site: https://github.com/yorevs/homesetup
+# License: Please refer to <https://opensource.org/licenses/MIT>
+
+__ssh_complete() {
+  
+  local suggestions=()
+  
+  [ "${#COMP_WORDS[@]}" != "2" ] && return 0
+  
+  suggestions=($(compgen -W "$(grep '^Host .*' ~/.ssh/config | cut -d ' ' -f2-)" -- "${COMP_WORDS[1]}"))
+  COMPREPLY=("${suggestions[@]}")
+  
+  return 0
+}
+
+complete -o default -F __ssh_complete ssh
--- dotfiles/bash/bash_completion.bash
+++ dotfiles/bash/bash_completion.bash
@@ -16,26 +16,26 @@ case "${HHS_MY_SHELL}" in
 
   bash)
     AUTO_CPL_D="${HHS_DIR}/bin"
-
+  
     # If bash_completion is installed source it.
     # FIXME: This auto-complete was commented out because it is hanging for some reason
     # if [[ -f /usr/local/etc/bash_completion ]]; then
     #   \. /usr/local/etc/bash_completion
     #   AUTO_CPL_TYPES+=('Bash_Completion')
     # fi
-
+  
     # Enable tab completion for `git`
     # Thanks to: https://github.com/git/git/tree/master/contrib/completion
-    if command -v git &> /dev/null; then
+    if command -v git &>/dev/null; then
       if [[ -f "$AUTO_CPL_D/git-completion.bash" ]]; then
         \. "$AUTO_CPL_D/git-completion.bash"
         AUTO_CPL_TYPES+=('Git')
       fi
     fi
-
+  
     # Enable tab completion for `docker`
     # Thanks to: Built in docker scripts. *Requires bash_complete
-    if command -v docker &> /dev/null && command -v _filedir &> /dev/null; then
+    if command -v docker &>/dev/null && command -v _filedir &>/dev/null; then
       if [[ -f "$AUTO_CPL_D/docker-compose-completion.bash" ]]; then
         \. "$AUTO_CPL_D/docker-compose-completion.bash"
         AUTO_CPL_TYPES+=('Docker-Compose')
@@ -49,57 +49,68 @@ case "${HHS_MY_SHELL}" in
         AUTO_CPL_TYPES+=('Docker')
       fi
     fi
-
+  
     # Enable tab completion for `gradle`.
     # Thanks to: https://github.com/gradle/gradle-completion
-    if command -v gradle &> /dev/null; then
+    if command -v gradle &>/dev/null; then
       if [[ -f "$AUTO_CPL_D/gradle-completion.bash" ]]; then
         \. "$AUTO_CPL_D/gradle-completion.bash"
         AUTO_CPL_TYPES+=('Gradle')
       fi
     fi
-
+  
     # Enable tab completion for `cf`.
     # Thanks to: https://github.com/cloudfoundry/cli/blob/master/ci/installers/completion/cf
-    if command -v cf &> /dev/null; then
+    if command -v cf &>/dev/null; then
       if [[ -f "$AUTO_CPL_D/pcf-completion.bash" ]]; then
         \. "$AUTO_CPL_D/pcf-completion.bash"
         AUTO_CPL_TYPES+=('PCF')
       fi
     fi
-
+  
     # Enable tab completion for `brew'.
     # Thanks to: https://github.com/Bash-it/bash-it/blob/master/completion/available/brew.completion.bash
-    if command -v brew &> /dev/null; then
+    if command -v brew &>/dev/null; then
       if [[ -f "$AUTO_CPL_D/brew-completion.bash" ]]; then
         \. "$AUTO_CPL_D/brew-completion.bash"
         AUTO_CPL_TYPES+=('Brew')
       fi
     fi
-
+  
     # Enable tab completion for `Kubernetes' CLI.
     # TODO Check if it is required to generate it on the fly.
-    if command -v kubectl &> /dev/null; then
+    if command -v kubectl &>/dev/null; then
       if [[ -f "$AUTO_CPL_D/kubectl-completion.bash" ]]; then
         \. "$AUTO_CPL_D/kubectl-completion.bash"
         AUTO_CPL_TYPES+=('Kubernetes')
       fi
     fi
-    
+  
     # Enable tab completion for `Helm' (Kubernetes Package Manager)
     # TODO Check if it is required to generate it on the fly.
-    if command -v help &> /dev/null; then
+    if command -v help &>/dev/null; then
       if [[ -f "$AUTO_CPL_D/helm-completion.bash" ]]; then
         \. "$AUTO_CPL_D/helm-completion.bash"
         AUTO_CPL_TYPES+=('Helm')
       fi
     fi
-
+  
     # Enable tab completion for `HomeSetup`.
     if [[ -f "$AUTO_CPL_D/hhs-completion.bash" ]]; then
       \. "$AUTO_CPL_D/hhs-completion.bash"
       AUTO_CPL_TYPES+=('HomeSetup')
     fi
+  
+    # Enable tab completion for `ssh`.
+    if [[ -f "$AUTO_CPL_D/ssh-completion.bash" ]]; then
+      \. "$AUTO_CPL_D/ssh-completion.bash"
+      AUTO_CPL_TYPES+=('ssh')
+    fi
+  
+    ;; # End of bash completions
+  
+  *)
+    echo "${HHS_MY_SHELL} shell does not include any cmpletions" >>"${HHS_WARNINGS_FILE}"
     ;;
 
 esac
