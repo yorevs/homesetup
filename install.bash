9be6097aba8f997dca6cdd51cec18c7251b8188a
Hugo Saporetti Junior <yorevs@gmail.com>
2020-05-08 22:57:02 -0300

    Changed Install improvements and hspm improvements due to sudo - [DEVELOP:{@hjunior}]
    Fixed install improvements - [DEVELOP:{@hjunior}]
    
    Fixed install improvements and hspm add -y flag - [DEVELOP:{@hjunior}]
    
    Fixed install improvements and hspm add -y flag - [DEVELOP:{@hjunior}]

--- install.bash
+++ install.bash
@@ -436,9 +436,12 @@ Usage: $APP_NAME [OPTIONS] <args>
       tools="${REQUIRED_TOOLS[*]//python/python3}"
       tools="${tools//pip/pip3}"
       REQUIRED_TOOLS=(${tools})
-      [[ ! -f '/usr/bin/python' ]] && ln -sf '/usr/bin/python3' '/usr/bin/python'
-      [[ ! -f '/usr/bin/pip' ]] && ln -sf '/usr/bin/pip3' '/usr/bin/pip'
     fi
+    # Link whatever python is available on the system
+    [[ ! -f '/usr/bin/python' && -f '/usr/bin/python3' ]] && ln -sf '/usr/bin/python3' '/usr/bin/python'
+    [[ ! -L '/usr/bin/python' && -f '/usr/bin/python2' ]] && ln -sf '/usr/bin/python2' '/usr/bin/python'
+    [[ ! -f '/usr/bin/pip' && -f '/usr/bin/pip3' ]] && ln -sf '/usr/bin/pip3' '/usr/bin/pip'
+    [[ ! -L '/usr/bin/pip' && -f '/usr/bin/pip2' ]] && ln -sf '/usr/bin/pip2' '/usr/bin/pip'
   }
 
   # Check installed tools
@@ -473,12 +476,12 @@ Usage: $APP_NAME [OPTIONS] <args>
     echo -e "${NC}"
     [[ -n "$ANS" ]] && echo ''
     if [[ "remote" == "${METHOD}" || "$ANS" == "y" || "$ANS" == 'Y' ]]; then
+      command -v sudo && SUDO=sudo
       echo ''
       echo -en "${WHITE}Installing [${MISSING_TOOLS[*]}] (${MY_OS}) ..."
       if [[ "Darwin" == "${MY_OS}" ]]; then
-        brew install ${MISSING_TOOLS[*]} &>/dev/null || quit 2 "Failed to install: ${MISSING_TOOLS[*]}"
+        ${SUDO} brew install ${MISSING_TOOLS[*]} &>/dev/null || quit 2 "Failed to install: ${MISSING_TOOLS[*]}"
       else
-        command -v sudo && SUDO=sudo
         if has "apt-get"; then
           ${SUDO} apt-get -y install ${MISSING_TOOLS[*]} || quit 2 "Failed to install: ${MISSING_TOOLS[*]}"
         elif has "yum"; then
