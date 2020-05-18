ea4b686a9639ab3145b459ff369f06718600a286
Hugo Saporetti Junior <yorevs@gmail.com>
2020-05-18 18:54:35 -0300

    Fixed hhs host-name - [DEVELOP:{@hjunior}]

--- bin/apps/bash/hhs-app/functions/ext-tools.bash
+++ bin/apps/bash/hhs-app/functions/ext-tools.bash
@@ -12,36 +12,42 @@
 function host-name() {
 
   local cur_hostn new_hostn
-
-  if [[ -z "${1}" ]]; then
+  
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    echo -e "Usage: ${FUNCNAME[0]} [new_hostname]"
+  elif [[ -z "${1}" ]]; then
     echo -en "${GREEN}Your current hostname is: ${PURPLE}"
     hostname
     quit $? "${NC}"
   else
-    cur_hostn=$(hostname)
-    new_hostn="${1}"
-    [[ -z "${new_hostn}" ]] && read -r -p "${YELLOW}Enter new hostname (ENTER to cancel): ${NC}" new_hostn
-    if [[ -n "${new_hostn}" && "${cur_hostn}" != "${new_hostn}" ]]; then
-      if [[ "$(uname -s)" == "Darwin" ]]; then
-        if sudo scutil --set HostName "${new_hostn}"; then
-          echo "${GREEN}Your new hostname has changed from \"${cur_hostn}\" to ${PURPLE}\"${new_hostn}\" ${NC} !"
-        else
-          quit 2 "Failed to change your hostname !"
-        fi
-      else
-        # Change the hostname in /etc/hosts & /etc/hostname
-        if sudo ised "s/${cur_hostn}/${new_hostn}/g" /etc/hosts && sudo ised "s/${cur_hostn}/${new_hostn}/g" /etc/hostname; then
-          echo "${GREEN}Your new hostname has changed from \"${cur_hostn}\" to ${PURPLE}\"${new_hostn}\" ${NC} !"
-          read -rn 1 -p "${YELLOW}Press 'y' key to reboot now: ${NC}" ANS
-          if [[ "$ANS" == "y" || "$ANS" == "Y" ]]; then
-            sudo reboot
+    if __hhs_has hostname; then
+      cur_hostn=$(hostname)
+      new_hostn="${1}"
+      [[ -z "${new_hostn}" ]] && read -r -p "${YELLOW}Enter new hostname (ENTER to cancel): ${NC}" new_hostn
+      if [[ -n "${new_hostn}" && "${cur_hostn}" != "${new_hostn}" ]]; then
+        if [[ "$(uname -s)" == "Darwin" ]]; then
+          if sudo scutil --set HostName "${new_hostn}"; then
+            echo "${GREEN}Your new hostname has changed from \"${cur_hostn}\" to ${PURPLE}\"${new_hostn}\" ${NC} !"
+          else
+            quit 2 "Failed to change your hostname !"
           fi
         else
-          quit 2 "Failed to change your hostname !"
+          # Change the hostname in /etc/hosts & /etc/hostname
+          if sudo ised "s/${cur_hostn}/${new_hostn}/g" /etc/hosts && sudo ised "s/${cur_hostn}/${new_hostn}/g" /etc/hostname; then
+            echo "${GREEN}Your new hostname has changed from \"${cur_hostn}\" to ${PURPLE}\"${new_hostn}\" ${NC} !"
+            read -rn 1 -p "${YELLOW}Press 'y' key to reboot now: ${NC}" ANS
+            if [[ "$ANS" == "y" || "$ANS" == "Y" ]]; then
+              sudo reboot
+            fi
+          else
+            quit 2 "Failed to change your hostname !"
+          fi
         fi
+      else
+        echo "${ORANGE}Your hostname hasn't changed !${NC}" && quit 1
       fi
     else
-      echo "${ORANGE}Your hostname hasn't changed !${NC}" && quit 1
+      quit 1 "You need 'hostname'installed to use this function !" 
     fi
   fi
 
