3c5903d9521198ca8c3a3fd0dde22cef2a22db69
Hugo Saporetti Junior <yorevs@gmail.com>
2023-04-20 17:44:31 -0300

    Fix prompts

--- docs/devel/bash-prompt-codes.md
+++ docs/devel/bash-prompt-codes.md
@@ -1,5 +1,13 @@
+Bash has four prompt strings that can be customized:
+
+PS0 is displayed after each command, before any output.
+PS1 is the primary prompt which is displayed before each command, thus it is the one most people customize.
+PS2 is the secondary prompt displayed when a command needs more input (e.g. a multi-line command).
+PS3 is not very commonly used. It is the prompt displayed for Bash's select built-in which displays interactive menus. Unlike the other prompts, it does not expand Bash escape sequences. Usually you would customize it in the script where the select is used rather than in your .bashrc.
+PS4 is also not commonly used. It is displayed when debugging bash scripts to indicate levels of indirection. The first character is repeated to indicate deeper levels.
+
 | Code       | Description                                                                                                                                                                         |
-| ---------- | ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+|------------|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 | \a         | an ASCII bell character (07)                                                                                                                                                        |
 | \d         | the date in "Weekday Month Date" format (e.g., "Tue May 26")                                                                                                                        |
 | \D{format} | the  format  is  passed to strftime(3) and the result is inserted into the prompt string; an empty format results in a locale-specific time representation. The braces are required |
--- dotfiles/bash/bash_prompt.bash
+++ dotfiles/bash/bash_prompt.bash
@@ -21,35 +21,30 @@ function __hhs_git_prompt() {
 
   local st_flag='' branch_name=''
 
-  # Check if the current directory is a Git repository.
-  if __hhs_has git && [[ "$(git rev-parse --is-inside-work-tree &> /dev/null && echo "${?}")" == '0' ]]; then
+  # check if the current directory is in .git before running git checks.
+  if [[ "$(git rev-parse --is-inside-git-dir 2>/dev/null)" == 'false' ]]; then
 
-    # check if the current directory is in .git before running git checks.
-    if [[ "$(git rev-parse --is-inside-git-dir 2>/dev/null)" == 'false' ]]; then
+    # Ensure the index is up to date.
+    git update-index --really-refresh -q &>/dev/null
 
-      # Ensure the index is up to date.
-      git update-index --really-refresh -q &>/dev/null
+    # Check for uncommitted changes in the index.
+    if ! git diff --quiet --ignore-submodules --cached; then
+      st_flag+='+'
+    fi
 
-      # Check for uncommitted changes in the index.
-      if ! git diff --quiet --ignore-submodules --cached; then
-        st_flag+='+'
-      fi
+    # Check for unstaged changes.
+    if ! git diff-files --quiet --ignore-submodules --; then
+      st_flag+='!'
+    fi
 
-      # Check for unstaged changes.
-      if ! git diff-files --quiet --ignore-submodules --; then
-        st_flag+='!'
-      fi
+    # Check for untracked files.
+    if [[ -n "$(git ls-files --others --exclude-standard)" ]]; then
+      st_flag+='?'
+    fi
 
-      # Check for untracked files.
-      if [[ -n "$(git ls-files --others --exclude-standard)" ]]; then
-        st_flag+='?'
-      fi
-
-      # Check for stashed files.
-      if git rev-parse --verify refs/stash &>/dev/null; then
-        st_flag+='$'
-      fi
-
+    # Check for stashed files.
+    if git rev-parse --verify refs/stash &>/dev/null; then
+      st_flag+='$'
     fi
 
     # Get the short symbolic ref. If HEAD isn’t a symbolic ref, get the short SHA for the latest commit
@@ -80,44 +75,48 @@ AT_ICN="${AT_ICN:-\357\207\272}"
 NET_ICN="${NET_ICN:-\357\233\277}"
 FOLDER_ICN="${FOLDER_ICN:-\357\201\273}"
 
-# Command history style.
-HIST_STYLE="\[${WHITE}\]${HIST_ICN} \!"
+# The history number of this command.
+HIST_STYLE="\[${WHITE}\]${HIST_ICN} \#"
 
-# Highlight the user name when logged in as root.
+# Logged username. Highlight when logged in as root.
 if [[ "${USER}" == "root" ]]; then
-  USER_STYLE="\[${WHITE}\] \[${ROOT_ICN}\]\[${RED}\] \u"
+  USER_STYLE="\[${WHITE}\] ${ROOT_ICN}\[${RED}\] \u"
 else
-  USER_STYLE="\[${WHITE}\] \[${USER_ICN}\]\[${GREEN}\] \u"
+  USER_STYLE="\[${WHITE}\] ${USER_ICN}\[${GREEN}\] \u"
 fi
 
-# Highlight the hostname when connected via SSH.
+# The hostname. Highlight when connected via SSH.
 if [[ "${SSH_TTY}" ]]; then
-  HOST_STYLE="\[${WHITE}\] \[${NET_ICN}\]\[${RED}\] \h"
+  HOST_STYLE="\[${WHITE}\] ${NET_ICN}\[${RED}\] \H"
 else
-  HOST_STYLE="\[${WHITE}\] \[${AT_ICN}\]\[${PURPLE}\] \h"
+  HOST_STYLE="\[${WHITE}\] ${AT_ICN}\[${PURPLE}\] \h"
 fi
 
-# Current directory path style.
-PATH_STYLE="\[${WHITE}\] \[${FOLDER_ICN}\]\[${ORANGE}\] \W"
+# Working directory base path.
+PATH_STYLE="\[${WHITE}\] ${FOLDER_ICN} \[${ORANGE}\] \W"
 
-# Git style
-GIT_STYLE="\[${WHITE}\]\$(__hhs_git_prompt \" \[${GIT_ICN}\] \[${CYAN}\]\")"
+# Git repository details.
+# Check if the current directory is a Git repository.
+if __hhs_has git && git rev-parse --is-inside-work-tree &>/dev/null; then
+  GIT_STYLE="\[${WHITE}\]\$(__hhs_git_prompt \" ${GIT_ICN} \[${CYAN}\]\")"
+else
+  GIT_STYLE=''
+fi
 
-# User prompt format
-PROMPT="\[${WHITE}\] \$\[${NC}\] "
+# User prompt format.
+PROMPT="\[${WHITE}\] \$ "
 
 # Set the terminal title and prompt.
-# Check ${HHS_HOME}/misc/prompt-codes.txt for more details
+# Check ${HHS_HOME}/docs/devel/bash-prompt-codes.md for more details
 
-# PS1 Style: Color and icons (default)
-PS1_STYLE="${HIST_STYLE}"  # The history number of this command
-PS1_STYLE+="${USER_STYLE}" # Logged username
-PS1_STYLE+="${HOST_STYLE}" # Hostname
-PS1_STYLE+="${PATH_STYLE}" # Working directory base path
-PS1_STYLE+="${GIT_STYLE}"  # Git repository details
-PS1_STYLE+="${PROMPT}"     # Prompt symbol
+#unset PROMPT_COMMAND
+PROMPT_COMMAND=''
 
-# PS2 Style: No icons, simple prompt.
-PS2_STYLE='\h:\u \W \$ '
+# PS1 Style: Color and icons (default).
+PS1_STYLE=$"${HIST_STYLE}${USER_STYLE}${HOST_STYLE}${PATH_STYLE}${GIT_STYLE}${PROMPT}"
 
-export PS1=${CUSTOM_PS:-$PS1_STYLE}
+# PS2 Style: Continuation prompt.
+PS2_STYLE=$'... '
+
+export PS1=${CUSTOM_PS1:-$PS1_STYLE}
+export PS2=${CUSTOM_PS2:-$PS2_STYLE}
