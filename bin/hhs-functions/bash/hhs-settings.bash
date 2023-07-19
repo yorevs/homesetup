d55db7b1ccb8e1c98858cec81184234c1eefa661
Hugo Saporetti Junior <yorevs@gmail.com>
2023-07-19 16:45:22 -0300

    New HomeSetup version '1.5.57': No commits

--- bin/hhs-functions/bash/hhs-settings.bash
+++ bin/hhs-functions/bash/hhs-settings.bash
@@ -14,53 +14,24 @@
 # shellcheck disable=SC1090
 function __hhs_settings() {
   
-  local version envs_file ret_val
+  local envs_file ret_val=0
   
-  version="$(python3 -m setman -v | tr -d '\n')"
-  
-  if [[ $# -eq 0 || "$1" == "-h" || "$1" == "--help" ]]; then
-      echo "Usage: ${FUNCNAME[0]} [-h] [-v] [-s] [-p] operation <args> ..."
-      echo " ____       _"
-      echo "/ ___|  ___| |_ _ __ ___   __ _ _ __"
-      echo "\___ \ / _ \ __| '_ \` _ \ / _\` | '_\ "
-      echo " ___) |  __/ |_| | | | | | (_| | | | |"
-      echo "|____/ \___|\__|_| |_| |_|\__,_|_| |_|"
-      echo ""
-      echo "HsPyLib Settings Manager ${version//setman /}- Manage your terminal settings."
-      echo ""
-      echo "    Options:"
-      echo "      -h, --help               show this help message and exit"
-      echo "      -v, --version            show program's version number and exit"
-      echo "      -s, --simple             display without formatting."
-      echo "      -p, --preserve           whether to preserve (no overwrite) existing settings."
-      echo ""
-      echo "    Arguments:"
-      echo "        operation : the operation to execute. One of "
-      echo "                    {get,set,del,list,search,truncate,import,export,source}"
-      echo "            get       : Retrieve the specified setting."
-      echo "            set       : Upsert the specified setting."
-      echo "            del       : Delete the specified setting."
-      echo "            list      : List in a table all settings matching criteria."
-      echo "            search    : Search and display all settings matching criteria."
-      echo "            truncate  : Clear all settings matching name."
-      echo "            import    : Import settings from a CSV formatted file."
-      echo "            export    : Export settings to a CSV formatted file."
-      echo "            source    : Source (bash export) all environment settings to current shell."
-      echo "}"
-    return 1
-  fi
-  
-  if [[ " ${*} " =~ " source " ]]; then
+  if [[ "${#}" -eq 0 ]]; then
+    python3 -m setman -h
+    ret_val=1
+  elif [[ " ${*} " =~ " source " ]]; then
     envs_file=$(mktemp)
-    python3 -m setman source > "${envs_file}"
+    python3 -m setman source -f "${envs_file}"
     ret_val=$?
   else
     python3 -m setman "${@}"
     ret_val=$?
   fi
-  
+
   [[ -n "${envs_file}" && -f "${envs_file}" ]] && source "${envs_file}"
   [[ -n "${envs_file}" && -f "${envs_file}" ]] && \rm -f "${envs_file}"
+  
+  echo -e "${NC}"
   
   return ${ret_val}
 }
