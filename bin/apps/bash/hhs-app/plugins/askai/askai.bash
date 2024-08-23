91ad9963527ac05b1bd5331ce202dd6e5d921801
Hugo Saporetti Junior <yorevs@gmail.com>
2024-08-22 21:44:21 -0300

    Fixing hhs askai

--- bin/apps/bash/hhs-app/plugins/askai/askai.bash
+++ bin/apps/bash/hhs-app/plugins/askai/askai.bash
@@ -44,32 +44,43 @@ function execute() {
 
   [[ -n "${AI_ENABLED}" ]] || quit 1 "AskAI is not installed. Visit ${ASKAI_URL} for installation instructions"
 
-  local args output ret_val ans script_name
+  local args output ans script_name
+
+  echo "${GREEN}Processing..."
 
   # shellcheck disable=SC2206
   args=("$@")
-  output=$(python3 -m askai -c off -p "${HHS_PROMPTS_DIR}/homesetup.txt" "${args[*]}")
+  output=$(python3 -m askai -c off -p "${HHS_PROMPTS_DIR}/homesetup.txt" "${args[@]}" 1>&1)
   [[ -z "${output}" ]] && echo -e "The query didn't produce an output!" && exit 1
-  script_name=$(echo "${output}" | grep -R '^# Script Name:')
-  [[ -z "${script_name}" ]] && echo -e "${output}\n" && exit 0
-  script_name="${script_name#*: }"
+  script_name=$(echo "${output}" | grep "^# [Ss]cript [Nn]ame:" | cut -d ':' -f2 | xargs)
+  if [[ -z "${script_name}" ]]; then
+    echo -e "${YELLOW}I was unable to detect the script name${NC}"
+    echo "${output}"
+    echo ''
+    quit 0
+  fi
+  script_name="${script_name#*:}"
   script_name="${script_name%.*}.bash"
 
-  if [[ -f "${script_name}" ]]; then
+  if [[ -s "${script_name}" ]]; then
     echo -e "${ORANGE}"
     read -rn 1 -p "File '${script_name}' already exists. Overwrite it (y/[n])? " ans
     echo -e "${NC}" && [[ -n "${ans}" ]] && echo ''
-  fi
-
-  if [[ -z "${ans}" || "${ans}" != "y" && "${ans}" != 'Y' ]]; then
-    echo -en "${BLUE}Writing file: ${WHITE}${script_name}${NC}... "
+    if [[ -n "${ans}" ]] && [[ "${ans}" == "y" || "${ans}" == 'Y' ]]; then
+      echo -en "${BLUE}Writing file: ${WHITE}${script_name}${NC}... "
+      echo "${output}" > "${script_name}"
+      [[ -s "${script_name}" ]] && echo "${GREEN}OK${NC}"
+      [[ -s "${script_name}" ]] || echo "${RED}FAILED${NC}"
+    fi
+  else
     echo "${output}" > "${script_name}"
-    [[ -s "${script_name}" ]] && echo "${GREEN}OK${NC}"
-    [[ -f "${script_name}" ]] || echo "${RED}FAILED${NC}"
   fi
 
-  ret_val=$?
-  echo -e "${NC}"
+  if [[ -s "${script_name}" ]]; then
+    chmod +x "${script_name}"
+    if __hhs_has bat; then bat "${script_name}"; else cat "${script_name}"; fi
+  fi
 
-  quit ${ret_val}
+  echo "${GREEN}Finished!${NC}"
+  quit 0
 }
