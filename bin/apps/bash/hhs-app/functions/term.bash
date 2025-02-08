d3f0af2e2e6c89ce12fbda2fd8b8528bd4e3e06c
Hugo Saporetti Junior <yorevs@gmail.com>
2025-02-08 01:09:53 -0300

    Fixed hhs shorts

--- bin/apps/bash/hhs-app/functions/term.bash
+++ bin/apps/bash/hhs-app/functions/term.bash
@@ -93,45 +93,40 @@ function shopts() {
 # @purpose: Display a table of terminal shortcuts.
 function shorts() {
   echo ''
-  echo "
-  +-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Terminal Shortcuts -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+
-  | Shortcut                | Description                                              |
-  |-------------------------+----------------------------------------------------------|
-  | Tab                     | Auto-complete command or file name                       |
-  | Ctrl + _                | Undo the last action                                     |
-  |-------------------------+----------------------------------------------------------|
-  | Ctrl + T (with FZF)     | Select a file using fzf TUI                              |
-  | Ctrl + R (with FZF)     | Search the history using fzf TUI                         |
-  |-------------------------+----------------------------------------------------------|
-  | Ctrl + A                | Move cursor to the beginning of the line                 |
-  | Ctrl + E                | Move cursor to the end of the line                       |
-  | Ctrl + K                | Cut text from the cursor to the end of the line          |
-  | Ctrl + U                | Cut text from the cursor to the beginning of the line    |
-  | Ctrl + Y                | Paste text that was cut with: Ctrl + K or Ctrl + U       |
-  | Ctrl + L                | Clear the terminal screen                                |
-  | Ctrl + C                | Cancel the current command                               |
-  | Ctrl + D                | Logout of the current shell                              |
-  | Ctrl + Z                | Suspend the current command                              |
-  | Ctrl + R (no fzf)       | Reverse search through command history                   |
-  | Ctrl + F                | Move cursor forward one character                        |
-  | Ctrl + B                | Move cursor backward one character                       |
-  | Ctrl + P                | Previous command in command history                      |
-  | Ctrl + N                | Next command in command history                          |
-  | Ctrl + S                | Stop output to the terminal                              |
-  | Ctrl + Q                | Resume output to the terminal                            |
-  | Ctrl + W                | Cut the word before the cursor                           |
-  | Ctrl + T (no fzf)       | Swap the last two characters before the cursor           |
-  | Ctrl + H                | Delete the character before the cursor                   |
-  | Ctrl + J                | Equivalent to Enter key                                  |
-  | Ctrl + V                | Insert a literal character (used for special characters) |
-  |-------------------------+--------------------------------------------------------- |
-  | Ctrl + X then Ctrl + E  | Edit command in the default editor                       |
-  | Ctrl + X then Ctrl + C  | Close the current window                                 |
-  | Ctrl + Shift + T        | Open a new terminal tab                                  |
-  | Ctrl + Shift + N        | Open a new terminal window                               |
-  |-------------------------+--------------------------------------------------------- |
-  | Alt +
-  " | __hhs_highlight '(Tab|Ctrl) [^|]*'
+  echo -e "
+  Tab                     \t Auto-complete command or file name
+  Ctrl + _                \t Undo the last action
+  Ctrl + T (with fzf)     \t Select a file using fzf TUI
+  Ctrl + R (with fzf)     \t Search the history using fzf TUI
+  Ctrl + A                \t Move cursor to the beginning of the line
+  Ctrl + E                \t Move cursor to the end of the line
+  Ctrl + K                \t Cut text from the cursor to the end of the line
+  Ctrl + U                \t Cut text from the cursor to the beginning of the line
+  Ctrl + Y                \t Paste text that was cut with: Ctrl + K or Ctrl + U
+  Ctrl + L                \t Clear the terminal screen
+  Ctrl + C                \t Cancel the current command
+  Ctrl + D                \t Logout of the current shell
+  Ctrl + Z                \t Suspend the current command
+  Ctrl + R (no fzf)       \t Reverse search through command history
+  Ctrl + F                \t Move cursor forward one character
+  Ctrl + B                \t Move cursor backward one character
+  Ctrl + P                \t Previous command in command history
+  Ctrl + N                \t Next command in command history
+  Ctrl + S                \t Stop output to the terminal
+  Ctrl + Q                \t Resume output to the terminal
+  Ctrl + W                \t Cut the word before the cursor
+  Ctrl + T (no fzf)       \t Swap the last two characters before the cursor
+  Ctrl + H                \t Delete the character before the cursor
+  Ctrl + J                \t Equivalent to Enter key
+  Ctrl + V                \t Insert a literal character (used for special characters)
+  Ctrl + X then Ctrl + E  \t Edit command in the default editor
+  Ctrl + X then Ctrl + C  \t Close the current window
+  Ctrl + Shift + T        \t Open a new terminal tab
+  Ctrl + Shift + N        \t Open a new terminal window
+  Alt + ←                 \t Navigate to the previous word
+  Alt + →                 \t Navigate to the next word
+  Alt + Backspace         \t Erase the previous word
+  " | nl | __hhs_highlight '  (Tab|Ctrl)([\\+ a-zA-Z]+)+'
 
   return 0
 }
