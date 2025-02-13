3aca419e26e6156ff6439428169246de14c1d070
Hugo Saporetti Junior <yorevs@gmail.com>
2025-02-13 00:01:42 -0300

    Add HomeBrew formula

---
+++ Formula/homesetup.rb
@@ -0,0 +1,17 @@
+class HomeSetup < Formula
+  desc "HomeSetup - The ultimate Terminal experience"
+  homepage "https://github.com/yorevs/homesetup"
+  url "https://github.com/yorevs/homesetup/archive/v1.8.22.tar.gz"
+  sha256 "your_tarball_sha256_here"
+  license "MIT"
+
+  depends_on "dependency" => :build
+
+  def install
+    system "bash", "-c", "curl -o- https://raw.githubusercontent.com/yorevs/homesetup/master/install.bash | bash"
+  end
+
+  test do
+    system "#{ENV['HOME']}/HomeSetup/bin/apps/bash/hhs-app/hhs.bash", "--version"
+  end
+end
