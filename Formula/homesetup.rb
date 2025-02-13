282295ad2f0c0d052030410d55ff4f9aad4e21e5
Hugo Saporetti Junior <yorevs@gmail.com>
2025-02-13 00:28:34 -0300

    Fix HomeBrew formula - 3 (sha)

--- Formula/homesetup.rb
+++ Formula/homesetup.rb
@@ -2,7 +2,7 @@ class Homesetup < Formula
   desc "HomeSetup - The ultimate Terminal experience"
   homepage "https://github.com/HS-Teams/homebrew-homesetup"
   url "https://github.com/HS-Teams/homebrew-homesetup/archive/v1.8.22.tar.gz"
-  sha256 "your_tarball_sha256_here"
+  sha256 "5a2f2c42f0038167fccdaf59cc8af59cc3aa7c2c2d0f3532e5724098196bff7f"
   license "MIT"
   head "https://github.com/HS-Teams/homebrew-homesetup.git", branch: "master"
 
